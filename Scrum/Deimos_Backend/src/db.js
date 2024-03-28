36a62838f492e65d44d7461bf3ea06416aece568
cor22982 <cor22982@uvg.edu.gt>
2024-03-28 15:28:41 -0600

    feaT: db.js funcion de registro agregada

--- Scrum/Deimos_Backend/src/db.js
+++ Scrum/Deimos_Backend/src/db.js
@@ -1,12 +1,10 @@
 // eslint-disable-next-line import/extensions
 import conn from './conn.js'
 
-export async function getAllPersonal() {
-  try {
-    const result = await conn.query('SELECT * FROM personal;');
-    return result.rows;
-  } catch (error) {
-    console.error('Error checking connection to PostgreSQL:', error);
-    return false;
+export async function register(pi,name, lastname,password_md5,age,type_user) {
+  const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,age,type_user])
+  if (type_user === 'usuario_comun'){
+    await conn.query('INSERT INTO userroles(id_role,pi) values (?,?);'[2,pi])
   }
+  return result.rows
 }
