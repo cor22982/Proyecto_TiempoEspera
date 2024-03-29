9c443683d63dc05602b7b7247071d765b3c111e4
cor22982 <cor22982@uvg.edu.gt>
2024-03-29 17:08:53 -0600

    Syntaxis correct

--- Scrum/backend/src/database/db.js
+++ Scrum/backend/src/database/db.js
@@ -4,7 +4,7 @@ import conn from '../connections/conn.js'
 export async function register(pi,name, lastname,password_md5,age,type_user) {
   const ageInt = parseInt(age, 10);
   console.log(isNaN(ageInt))
-  const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,ageInt,type_user]) 
+  const result = await conn.query('INSERT INTO users (pi, name, lastname, password, age, type_user) VALUES ($1, $2, $3, $4, $5, $6);', [pi, name, lastname, password_md5, ageInt, type_user]);
   console.log(result.rows)
   return result.rows
 }
