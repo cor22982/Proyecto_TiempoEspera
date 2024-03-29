4687470ed0a0598edff0c7d088e6f52639686b89
cor22982 <cor22982@uvg.edu.gt>
2024-03-29 17:45:38 -0600

    fix userroles

--- Scrum/backend/src/database/db.js
+++ Scrum/backend/src/database/db.js
@@ -5,9 +5,9 @@ export async function register(pi,name, lastname,password_md5,age,type_user) {
   const ageInt = parseInt(age, 10);
   const result = await conn.query('INSERT INTO users (pi, name, lastname, password, age, type_user) VALUES ($1, $2, $3, $4, $5, $6);', [pi, name, lastname, password_md5, ageInt, type_user]);
   if (type_user==='usuario_comun'){
-    await conn.query('INSERT INTO userroles (id_role,pi) values (2,$1)',[pi]);
+    await conn.query('INSERT INTO userroles (id_role,pi) values (2,$1);',[pi]);
   }
-  if (type_user==='empleador'){
+  else if (type_user==='empleador'){
     await conn.query('INSERT INTO userroles (id_role,pi) VALUES (3,$1);',[pi]);
   }
   return result.rows
