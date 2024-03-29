568490668a28846afa7b7741ef1af5d202f15d6a
cor22982 <cor22982@uvg.edu.gt>
2024-03-29 16:55:53 -0600

    fix_ sintaxys

--- Scrum/backend/src/database/db.js
+++ Scrum/backend/src/database/db.js
@@ -4,7 +4,7 @@ import conn from '../connections/conn.js'
 export async function register(pi,name, lastname,password_md5,age,type_user) {
   const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,age,type_user])
   if (type_user === 'usuario_comun'){
-    await conn.query('INSERT INTO userroles (id_role, pi) VALUES (?, ?);', [2, pi]);
+    await conn.query('INSERT INTO userroles (id_role, pi) VALUES (2, ?);', [pi]);
   }
   return result.rows
 }
