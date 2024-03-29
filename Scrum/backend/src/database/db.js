e3ebd3d71289d00e1409ab1e95f13c8fca3b7884
cor22982 <cor22982@uvg.edu.gt>
2024-03-29 17:03:21 -0600

    fix

--- Scrum/backend/src/database/db.js
+++ Scrum/backend/src/database/db.js
@@ -3,6 +3,8 @@ import conn from '../connections/conn.js'
 
 export async function register(pi,name, lastname,password_md5,age,type_user) {
   const ageInt = parseInt(age, 10);
+  console.log(isNaN(ageInt))
   const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,ageInt,type_user]) 
+  console.log(result.rows)
   return result.rows
 }
