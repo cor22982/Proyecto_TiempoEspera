d1b7aff692afb008a79eef297df9c6088a999953
cor22982 <cor22982@uvg.edu.gt>
2024-03-29 17:01:21 -0600

    fix_error

--- Scrum/backend/src/database/db.js
+++ Scrum/backend/src/database/db.js
@@ -2,7 +2,7 @@
 import conn from '../connections/conn.js'
 
 export async function register(pi,name, lastname,password_md5,age,type_user) {
-  const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,age,type_user])
-  
+  const ageInt = parseInt(age, 10);
+  const result = await conn.query('INSERT INTO users (pi,name,lastname,password,age,type_user) VALUES (?,?,?,?, ?,?);', [pi,name, lastname,password_md5,ageInt,type_user]) 
   return result.rows
 }
