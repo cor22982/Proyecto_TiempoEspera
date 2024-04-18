8384d06ebd4a3b50de35098d58c72ecee6ca4b1c
cor22982 <cor22982@uvg.edu.gt>
2024-04-18 01:05:28 -0600

    feat: Pantalla inicio

--- Scrum/fronted/src/Components/SearchInput.jsx
+++ Scrum/fronted/src/Components/SearchInput.jsx
@@ -1,8 +1,15 @@
 import './SearchInput.css'
-const SearchInput = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const SearchInput = ({icono, placeholder,onChange,value}) => {
   return (
     <div className='searched'>
-
+      <FontAwesomeIcon icon={icono} className="icon-serchad" />
+      
+      <input
+          placeholder={placeholder} 
+          onChange={({ target: { value }}) => onChange(value)}
+          value={value}
+          className='input-search'></input>
     </div>
   )
 }
--- Scrum/fronted/src/Pages/Pages.jsx
+++ Scrum/fronted/src/Pages/Pages.jsx
@@ -1,6 +1,6 @@
 import './Pages.css'
 import MenuButton from '@components/MenuButton';
-import { faUser, faHome, faSave, faGear, faBell, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faHome, faSave, faGear, faBell, faRightFromBracket, faSearch } from '@fortawesome/free-solid-svg-icons';
 import '../assets/Login/logotipo.png'
 import SearchInput from '@components/SearchInput';
 const Pages = () => {
@@ -9,20 +9,20 @@ const Pages = () => {
       <div className='opciones'>
         <img src='../src/assets/Login/logotipo.png' className='imagen-portada'></img>
         <MenuButton nombre='Principal' icono={faHome}></MenuButton>
-        <div style={{width: '200px' ,height: '0px', border: '1px solid #FFFFFF'}}></div>
+        <div style={{width: '150px' ,height: '0px', border: '1px solid #FFFFFF'}}></div>
         <MenuButton nombre='Cuenta' icono={faUser}></MenuButton>
         <MenuButton nombre='Guardados' icono={faSave}></MenuButton>
         <MenuButton nombre='Configuracion' icono={faGear}></MenuButton>
         <MenuButton nombre='Notificaciones' icono={faBell}></MenuButton>
         <br></br> 
-        <br></br> 
+       
         <br></br>   
         <MenuButton nombre='Salir' icono={faRightFromBracket}></MenuButton>
       </div>
 
       <div className='interfaz'>
          <div className='cabecera'>
-          <SearchInput></SearchInput>
+          <SearchInput icono={faSearch}></SearchInput>
          </div>
          <div className='cuerpo'>
           
