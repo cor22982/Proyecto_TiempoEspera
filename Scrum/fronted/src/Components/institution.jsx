002f3b9a92d568c53cbccde0b56b05361b4f23e1
ppguz <ppguzmanmayen@gmail.com>
2024-04-18 21:36:52 -0600

    Feat: add structure for institution component

---
+++ Scrum/fronted/src/Components/institution.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './institution.css';
+
+const insttutionComponent = ({name, image, onClick, tiempo}) =>{
+    return(
+        <div className="container" onClick= {onClick}>
+            <div className = "image-container">
+                <img src={image} alt="institution" className="institution-image" onClick={onClick}/>
+            </div>
+            <div className = 'info-container'>
+                <h1 className="heading">{name}</h1>
+                <h2 className = "timetitle">Tiempo Promedio</h2>
+                <h3 className="time">{tiempo}</h3>
+            </div>
+
+        </div>
+    );
+}
+
+insttutionComponent.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    tiempo: PropTypes.string.isRequired
+}
+
+export default insttutionComponent;
