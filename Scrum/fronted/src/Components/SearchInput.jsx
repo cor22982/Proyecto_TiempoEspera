b412ddd5d9f03e8101109333625e05182818e06a
cor22982 <cor22982@uvg.edu.gt>
2024-04-19 05:17:23 -0600

    feat: Finish page main

--- Scrum/fronted/src/Components/SearchInput.jsx
+++ Scrum/fronted/src/Components/SearchInput.jsx
@@ -1,6 +1,12 @@
 import './SearchInput.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const SearchInput = ({icono, placeholder,onChange,value}) => {
+const SearchInput = ({icono, placeholder,onChange,value, onpressenter}) => {
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      // Llama a la función que quieres ejecutar aquí
+      onpressenter()
+    }
+  };
   return (
     <div className='searched'>
       <FontAwesomeIcon icon={icono} className="icon-serchad" />
@@ -9,7 +15,8 @@ const SearchInput = ({icono, placeholder,onChange,value}) => {
           placeholder={placeholder} 
           onChange={({ target: { value }}) => onChange(value)}
           value={value}
-          className='input-search'></input>
+          className='input-search'
+          onKeyDown={handleKeyPress}></input>
     </div>
   )
 }
--- Scrum/fronted/src/User_Common/Page_Main.jsx
+++ Scrum/fronted/src/User_Common/Page_Main.jsx
@@ -1,25 +1,57 @@
 import InsttutionComponent from "../Components/InsttutionComponent"
 import SearchInput from "../Components/SearchInput"
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react'
 import './Page_Main.css'
 const Page_Main = () => {
+  const [busqueda, setBusqueda] = useState('')
+  const [datos , setDatos] = useState([]);
 
+  const onSearch = async () => {
+    const busquedaParseada = busqueda.toLowerCase().replace(/ /g, '%20');
+    console.log(busquedaParseada)
+    const fetchOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const response = await fetch(`https://deimoss.web05.lol/institutions/${busqueda}`, fetchOptions)
+    if (response.ok) {
+      const data = await response.json();
+      setDatos(data)
+      return  
+    }
+    setDatos([])
+  }
   const onclick = () => {
 
   } 
   return (
     <div className="contenido">
       <div className='cabecera'>
-        <SearchInput icono={faSearch} placeholder='Buscar tramite'></SearchInput>
+        <SearchInput 
+          icono={faSearch} 
+          placeholder='Buscar tramite'
+          value={busqueda}
+          onChange={(value) => setBusqueda(value)}
+          onpressenter={onSearch}
+          ></SearchInput>
       </div>
 
       <div className="componentes">
-        <InsttutionComponent
-          name= "RENAP zona 1" 
-          image= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZadqAgnFLnzDl0RncSwvEo3z8wKa3Thvsn0tZISkCtQ&s  " 
-          onClick = {onclick}
-          tiempo = "0:00"/>
 
+        {
+          datos.map((dato, index) => (
+            <InsttutionComponent
+            key={index}
+            name={dato.name}
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZadqAgnFLnzDl0RncSwvEo3z8wKa3Thvsn0tZISkCtQ&s"
+            onClick={onclick}
+            tiempo="0:00"
+          />
+          ))
+        }
         
       </div>
       
