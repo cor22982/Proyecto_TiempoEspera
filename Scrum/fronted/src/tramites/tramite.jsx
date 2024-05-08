07f7dc18560d1e41d1923236c04ff79745c2c665
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-05-08 07:39:39 -0600

    feat: an image component was made to be used in processing

---
+++ Scrum/fronted/src/tramites/tramite.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faStarHalf, faStar, faComment } from '@fortawesome/free-solid-svg-icons'
+import './tramite.css'
+
+const Tramite = () => {
+    return (
+
+        <div className='tramite-container'>
+            
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Tramite;
