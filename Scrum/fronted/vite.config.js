85cd428a65c78bb06d414bd0872949cc007a1bd2
cor22982 <cor22982@uvg.edu.gt>
2024-05-10 15:20:35 -0600

    feat: Add server name

--- Scrum/fronted/vite.config.js
+++ Scrum/fronted/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-    port: 3000
-  },
   resolve: {
     alias: {
       '@components': '/src/Components',
