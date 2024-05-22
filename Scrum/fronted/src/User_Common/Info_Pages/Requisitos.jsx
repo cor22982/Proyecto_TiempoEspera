1282f9e07ef767db21a2e6598fc7d584026c9aa1
cor22982 <cor22982@uvg.edu.gt>
2024-05-21 19:04:25 -0600

    feat: Add steps

--- Scrum/fronted/src/User_Common/Info_Pages/Requisitos.jsx
+++ Scrum/fronted/src/User_Common/Info_Pages/Requisitos.jsx
@@ -1,33 +1,45 @@
 import useApi from '@hooks/useApi';
-import Checkbox from "../../Components/Checkbox"  
-import { useEffect,useState } from 'react';
-const Requisitos = ({data}) => {
-  const { id_procedure } = data
-  const {  llamadowithoutbody } = useApi(`https://deimoss.web05.lol/requirements/${id_procedure}`);
-  const [requeriments , setRequeriments] = useState([]);
- 
+import Checkbox from "../../Components/Checkbox";
+import { useEffect, useState } from 'react';
+
+const Requisitos = ({ data }) => {
+  const { id_procedure } = data;
+  const { llamadowithoutbody } = useApi(`https://deimoss.web05.lol/requirements/${id_procedure}`);
+  const [requirements, setRequirements] = useState([]);
+  const [pasos, setPasos] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
-      const requisitos = await llamadowithoutbody('GET');
-      setRequeriments(requisitos)
+      const data_get = await llamadowithoutbody('GET');
+      setRequirements(data_get.requirements);
+      setPasos(data_get.steps[0].pasos); // Acceder al array de pasos dentro de steps
     };
 
     fetchData();
   }, [llamadowithoutbody]);
 
   return (
-    <div>
-      <h4>Requisitos</h4>
-      {
-        requeriments.map( (req, index) => (
-          <Checkbox 
-            key={index}
-            name={req.name +" ( "+ req.description+" )"}/>
-        ))
-      }
+    <div style={{ overflowY: 'auto', maxHeight: '600px' }}>
+      <h4>Pasos</h4>
       
+        {pasos.map((paso, index) => (
+          
+          
+          <Checkbox
+            key={index}
+            name={paso}
+        />
+        ))}
+      
+      <h4>Documentos</h4>
+      {requirements.map((req, index) => (
+        <Checkbox
+          key={index}
+          name={req.name + " (" + req.description + ")"}
+        />
+      ))}
     </div>
-  ) 
-}
+  );
+};
 
-export default Requisitos
+export default Requisitos;
