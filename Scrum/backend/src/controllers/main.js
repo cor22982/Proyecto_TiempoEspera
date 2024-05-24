131df3250d898f714cf6ea9a879c6bb7085ea017
ppguz <ppguzmanmayen@gmail.com>
2024-05-23 21:50:51 -0600

    Feat: endpoint for inserting new rating

--- Scrum/backend/src/controllers/main.js
+++ Scrum/backend/src/controllers/main.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { validationResult } from 'express-validator';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { register, getProcedureInfo, getAllInstitutionInfo, getProcedureRequierements, getInstitutionByID, getComments, createComment, getsteps, getUserByPi, getRating} from '../database/db.js';
+import { register, getProcedureInfo, getAllInstitutionInfo, getProcedureRequierements, getInstitutionByID, getComments, createComment, getsteps, getUserByPi, getRating, insertNewRating} from '../database/db.js';
 import { getUserLoginInfo } from '../database/auth.js';
 import { generateToken, decodeToken } from './jwt.js';
 
@@ -173,6 +173,18 @@ app.get('/rating/:id_institution', async (req, res) => {
   catch(error){
     console.error('Error en la búsqueda de comentarios:', error);
     res.status(500).send('Error del servidor :(');
+  }
+});
+
+app.post('/rating', async (req, res) => {
+  try {
+    const { institution, rating, pi } = req.body;
+    await insertNewRating(institution, rating, pi);
+    res.status(200).json({ succes: true });
+  }
+  catch(error){
+    console.error('Error al crear rating:', error);
+    res.status(500).json({ succes: false });
   }
 });
 
