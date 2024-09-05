d75a1c37ba576a2ee8ea7d078ab6f883587bb8cd
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2024-09-05 13:59:58 -0600

    agregado de edad dependiendo del año dado

--- Scrum/backend/src/controllers/main.js
+++ Scrum/backend/src/controllers/main.js
@@ -32,7 +32,19 @@ const validateRequest = (req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello from API PROYECTO DEIMOS');
 });
-
+//Metodo de asignación de edad
+/*
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+*/
 // Registro de usuario
 app.post('/register', validateRequest, async (req, res) => {
   console.log("body", req.body);
