a69e5f294a383067fc972979af5d59fa636351cc
ppguz <ppguzmanmayen@gmail.com>
2024-09-27 18:37:18 -0600

    Refact: replace temp with query for comments endpoint

--- Scrum/backend/src/controllers/main.js
+++ Scrum/backend/src/controllers/main.js
@@ -174,9 +174,7 @@ app.get('/requirements/:id_procedure', async (req, res) => {
 
 app.get('/comments/:id_institution', async (req, res) => {
   try {
-    const { id_institution } = req.params;
-    const comments = await getComments(id_institution);
-    res.status(200).json(comments);
+    res.status(200).json(await getComments(req.params.id_institution));
   }
   catch(error){
     console.error('Error en la búsqueda de comentarios:', error);
