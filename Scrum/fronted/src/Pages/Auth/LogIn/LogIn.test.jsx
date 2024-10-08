a7a7c98a8b2a1b6e5b290a2c7846e9072a8a7fb8
cor22982 <cor22982@uvg.edu.gt>
2024-10-08 14:56:41 -0600

    feaT; Fix dev

--- Scrum/fronted/src/Pages/Auth/LogIn/LogIn.test.jsx
+++ Scrum/fronted/src/Pages/Auth/LogIn/LogIn.test.jsx
@@ -1,7 +1,7 @@
 import { test, expect, vi } from 'vitest';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Login from './Login';
+import Login from './LogIn';
 
 // Mockea useFormLogin
 vi.mock('@hooks/useFormLogin', () => ({
