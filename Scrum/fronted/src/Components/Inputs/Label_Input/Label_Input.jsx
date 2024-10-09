06dfb4a03c02fda88cb1b6c5858b8c51f5eb8f9a
cor22982 <cor22982@uvg.edu.gt>
2024-10-08 22:35:53 -0600

    feat: Add labels

--- Scrum/fronted/src/Components/Inputs/Label_Input/Label_Input.jsx
+++ Scrum/fronted/src/Components/Inputs/Label_Input/Label_Input.jsx
@@ -4,20 +4,32 @@ import { faPen, faCheck, faX } from '@fortawesome/free-solid-svg-icons';
 import './Label_Input.css'
 
 
-const Label_Input=({label_name})=>{
+const Label_Input=({label_name, onChange,value, to_Send})=>{
   const [show_input, set_ShowInput] = useState(false)
+  const onPush = async({label}) => {
+    await  to_Send(label)
+    set_ShowInput(!show_input)
+  }
   return(
     <div >
       {show_input ? (
         <div style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
-          <input type="text" placeholder={label_name} />
+          <input 
+            type="text" 
+            placeholder={label_name} 
+            onChange={({ target: { value }}) => onChange(value)}
+            value={value}/>
           <div style={{display: 'flex', flexDirection: 'row', gap:'10px', alignItems: 'center'}}>
-              <div className='checks-buttons' style={{'--icon-color': 'green'}}>
+              <div 
+                className='checks-buttons' 
+                style={{'--icon-color': 'green'}}>
                 <FontAwesomeIcon
                   icon={faCheck}
                 />
               </div>
-              <div className='checks-buttons' style={{'--icon-color': 'red'}}>
+              <div className='checks-buttons' 
+                   style={{'--icon-color': 'red'}}
+                   onClick={()=>{set_ShowInput(!show_input)}}>
                 <FontAwesomeIcon
                   icon={faX}
                 />
