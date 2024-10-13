e9316d621e81119c36249d616f1dacb2850fb849
cor22982 <cor22982@uvg.edu.gt>
2024-10-13 01:57:57 -0600

    fix: Fixing date

--- Scrum/backend/src/controllers/main.js
+++ Scrum/backend/src/controllers/main.js
@@ -258,7 +258,7 @@ app.post('/newAppointment', async (req, res) => {
     
     const [day, month, year] = req.body.date.split('-');
     const dateString = `${year}-${month}-${day}T${req.body.time}:00`;
-    date_notifi = new Date(dateString)
+    const date_notifi = new Date(dateString)
     console.log(date_notifi)
     const response = await client.createNotification(notification);
 
