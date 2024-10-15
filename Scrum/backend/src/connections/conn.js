cb372611a6b0a98604e13dba93a842da95ff7e5a
ppguz <ppguzmanmayen@gmail.com>
2024-10-15 12:52:49 -0600

    Fix: error in env variable names

--- Scrum/backend/src/connections/conn.js
+++ Scrum/backend/src/connections/conn.js
@@ -7,11 +7,11 @@ dotenv.config({ path: '../../../.env' });
 const { Client } = pg;
 
 const client = new Client({
-  user: process.env.DB_OWNER_USER,
-  host: process.env.DB_OWNER_HOST,
-  database: process.env.DB_OWNER_DATABASE,
-  password: process.env.DB_OWNER_PASSWORD,
-  port: parseInt(process.env.DB_OWNER_PORT, 10),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT, 10),
 });
 
 client.connect()
