26568f9f8b1b8585198ab92550028c2cb2e3bf38
ppguz <ppguzmanmayen@gmail.com>
2024-10-15 19:25:33 -0600

    Fix: error in recipent email

--- Scrum/backend/src/controllers/main.js
+++ Scrum/backend/src/controllers/main.js
@@ -252,12 +252,13 @@ app.get('/rating/:id_institution', async (req, res) => {
 app.post('/passwordRequest', async (req, res) =>{
   try {
     const OTP = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000
-    const email = await getUserEmail(req.body.pi)
+    const result = await getUserEmail(req.body.pi)
     console.log(email)
     console.log(email.email)
+    console.log(result[0].email)
     const mail_options = {
       from: 'deimosgt502@gmail.com',       
-      to: email.email,          
+      to: result[0].email,          
       subject: 'Cambio de contrraseña',    
       text: 'Has solicitado un cambio de contraseña, este es tu código de verificación',  
       html: `<h1>Tu código de verificación es: </h1><p>${OTP}</p>` 
