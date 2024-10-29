0c62f04fac3c2ce0bf3ac4c5121fe787ed1bc4cf
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-10-29 10:15:59 -0600

    fix: arreglo de busqueda de tarje con un click

--- Scrum/fronted/src/Pages/Home/HomeMain/HomeMain.jsx
+++ Scrum/fronted/src/Pages/Home/HomeMain/HomeMain.jsx
@@ -1,58 +1,68 @@
-import './HomeMain.css';
-import Principal from '@pages/Home/HomeSearch/HomeSearch';
-import useApi from '@hooks/api/useApi';
-import { useState } from 'react';
-import  Informacion from '@pages/Institution/InstitutionRoutes';
-import SearchInput from '@components/Inputs/SearchInput';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import "./HomeMain.css";
+import Principal from "@pages/Home/HomeSearch/HomeSearch";
+import useApi from "@hooks/api/useApi";
+import { useState, useEffect } from "react";
+import Informacion from "@pages/Institution/InstitutionRoutes";
+import SearchInput from "@components/Inputs/SearchInput";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Page_Main = ({ pi }) => {
-  const [busqueda, setBusqueda] = useState('');
+  const [busqueda, setBusqueda] = useState("");
   const [pantalla, setPantalla] = useState(true);
-  const { llamadowithoutbody } = useApi(`https://deimoss.web05.lol/institutions/${busqueda}`);
+  const { llamadowithoutbody } = useApi(
+    `https://deimoss.web05.lol/institutions/${busqueda}`
+  );
   const [datos, setDatos] = useState([]);
   const [object_datos, setobject_datos] = useState(null);
   const [seeScreens, setSeeScren] = useState(false);
 
   const sugerenciasTramites = [
-    'Cita controlada',
-    'Solicitud Electrónica de NIT',
-    'Inscripción NIT sin Obligaciones con invalidez física',
-    'Inscripción de sociedad',
-    'Renovación de pasaporte',
-    'Licencia de conducción',
-    'Cambio de nombre',
-    'Actualización de cédula'
+    "Cita controlada",
+    "Solicitud Electrónica de NIT",
+    "Inscripción NIT sin Obligaciones con invalidez física",
+    "Inscripción de sociedad",
+    "Renovación de pasaporte",
+    "Licencia de conducción",
+    "Cambio de nombre",
+    "Actualización de cédula",
   ];
 
-  const fetchData = async () => {
-    if (busqueda.trim() === '') {
-      return;
+  // Usar useEffect para ejecutar fetchData cuando `busqueda` cambie
+  useEffect(() => {
+    if (busqueda.trim() !== "") {
+      fetchData();
     }
+  }, [busqueda]);
 
-    const busquedaParseada = busqueda.toLowerCase().replace(/ /g, '%20');
+  const fetchData = async () => {
+    const busquedaParseada = busqueda.toLowerCase().replace(/ /g, "%20");
     try {
-      const response = await llamadowithoutbody('GET', `https://deimoss.web05.lol/institutions/${busquedaParseada}`);
+      const response = await llamadowithoutbody(
+        "GET",
+        `https://deimoss.web05.lol/institutions/${busquedaParseada}`
+      );
       setDatos(response);
       setSeeScren(true);
+      console.log("Nuevo estado de `datos` después de setDatos:", response);
     } catch (e) {
+      console.log("Error en la solicitud:", e);
       setDatos([]);
     }
   };
 
   return (
-    <div className='contenido'>
-      <div className='cabecera'>
-        <SearchInput 
-          icono={faSearch} 
-          placeholder='Buscar tramite'
-          value={busqueda} 
+    <div className="contenido">
+      <div className="cabecera">
+        <SearchInput
+          icono={faSearch}
+          placeholder="Buscar tramite"
+          value={busqueda}
           onChange={(value) => setBusqueda(value)}
           onpressenter={fetchData}
           suggestions={sugerenciasTramites}
         />
-      </div> 
-      <div className='paginas'>
+      </div>
+      <div className="paginas">
         {pantalla ? (
           seeScreens ? (
             <Principal
@@ -68,37 +78,31 @@ const Page_Main = ({ pi }) => {
               <div className="tramites-grid">
                 <div
                   className="box-search red"
-                  onClick={() => {
-                    setBusqueda('Cita controlada');
-                    fetchData(); // Realiza la búsqueda al hacer clic
-                  }}
+                  onClick={() => setBusqueda("Cita controlada")}
                 >
                   <span>Cita controlada</span>
                 </div>
                 <div
                   className="box-search blue"
-                  onClick={() => {
-                    setBusqueda('Solicitud Electrónica de NIT');
-                    fetchData(); // Realiza la búsqueda al hacer clic
-                  }}
+                  onClick={() => setBusqueda("Solicitud Electrónica de NIT")}
                 >
                   <span>Solicitud Electrónica de NIT</span>
                 </div>
                 <div
                   className="box-search yellow"
-                  onClick={() => {
-                    setBusqueda('Inscripción NIT sin Obligaciones con invalidez física');
-                    fetchData(); // Realiza la búsqueda al hacer clic
-                  }}
+                  onClick={() =>
+                    setBusqueda(
+                      "Inscripción NIT sin Obligaciones con invalidez física"
+                    )
+                  }
                 >
-                  <span>Inscripción NIT sin Obligaciones con invalidez física</span>
+                  <span>
+                    Inscripción NIT sin Obligaciones con invalidez física
+                  </span>
                 </div>
                 <div
                   className="box-search green"
-                  onClick={() => {
-                    setBusqueda('Inscripción de sociedad');
-                    fetchData(); // Realiza la búsqueda al hacer clic
-                  }}
+                  onClick={() => setBusqueda("Inscripción de sociedad")}
                 >
                   <span>Inscripción de sociedad</span>
                 </div>
