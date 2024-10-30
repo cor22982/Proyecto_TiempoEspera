7bf6c98a4aec96f66fceb82e50a2a84f5a6a3710
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-10-29 18:15:01 -0600

    refactor: modularizado de custombutton

--- Scrum/fronted/src/Components/Buttons/DropDownCustom/DropDownCustom.jsx
+++ Scrum/fronted/src/Components/Buttons/DropDownCustom/DropDownCustom.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import './DropDownCustom.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import "./DropDownCustom.css";
 
-const Dropdowncustom = ({ nombre, lista,onChange }) => {
+const Dropdowncustom = ({ nombre, lista, onChange }) => {
   const [dropdown, setDropdown] = useState(false);
   const [tituloSeleccionado, setTituloSeleccionado] = useState(nombre);
 
@@ -13,17 +18,21 @@ const Dropdowncustom = ({ nombre, lista,onChange }) => {
 
   const seleccionarItem = (item) => {
     setTituloSeleccionado(item);
-    onChange(item)
+    onChange(item);
     setDropdown(false); // Cerrar el Dropdown después de seleccionar un item
   };
 
   return (
-    <div className='dropdown-container'>
+    <div className="dropdown-container">
       <Dropdown isOpen={dropdown} toggle={abrirCerrar}>
-        <DropdownToggle className='dropdown-toggle'>{tituloSeleccionado}</DropdownToggle>
+        <DropdownToggle className="dropdown-toggle">
+          {tituloSeleccionado}
+        </DropdownToggle>
         <DropdownMenu>
           {lista.map((item, index) => (
-            <DropdownItem key={index} onClick={() => seleccionarItem(item)}>{item}</DropdownItem>
+            <DropdownItem key={index} onClick={() => seleccionarItem(item)}>
+              {item}
+            </DropdownItem>
           ))}
         </DropdownMenu>
       </Dropdown>
