0939dbabf9312ef5fe3abfe91775c8e280552a22
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-10-30 01:10:45 -0600

    refactor: modularizacion y parametrizacion de  componentes UI

--- Scrum/fronted/src/Components/UI/Image/Image.jsx
+++ Scrum/fronted/src/Components/UI/Image/Image.jsx
@@ -1,28 +1,25 @@
-import './Image.css'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Image.module.css";
 
 const ImageComponent = ({ src, alt, onClick }) => {
   const handleClick = () => {
     if (onClick) {
-      onClick()
+      onClick();
     }
-  }
+  };
 
   return (
-    <div className="image-container" onClick={handleClick}>
-      <img
-        src={src}
-        alt={alt}
-        className="image"
-      />
+    <div className={styles.imageContainer} onClick={handleClick}>
+      <img src={src} alt={alt} className={styles.image} />
     </div>
-  )
-}
+  );
+};
 
 ImageComponent.propTypes = {
   onClick: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
-}
+  alt: PropTypes.string.isRequired,
+};
 
-export default ImageComponent
+export default ImageComponent;
