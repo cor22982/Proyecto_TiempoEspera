d7509e3601e9731b92e27f9b028afa00bfda9ef6
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-10-29 19:08:03 -0600

    refactor: iconbutton refactor y parametrizado e inicio de edicion en insititutionroutes para uso de iconbutton

--- Scrum/fronted/src/Components/Buttons/IconButton/IconButton.jsx
+++ Scrum/fronted/src/Components/Buttons/IconButton/IconButton.jsx
@@ -1,18 +1,25 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './IconButton.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./IconButton.module.css";
 
 function IconButton({ icono, onclick, color, texto }) {
   return (
-    <div onClick={onclick} style={{display: 'flex' , gap: '10px'}} className='icon_button'>
-       {texto && <span className="icon_text" style={{ '--icon-color': color }}>{texto}</span>}
+    <div
+      onClick={onclick}
+      style={{ display: "flex", gap: "10px" }}
+      className={styles["icon_button"]}
+    >
+      {texto && (
+        <span className={styles["icon_text"]} style={{ "--icon-color": color }}>
+          {texto}
+        </span>
+      )}
       <FontAwesomeIcon
         icon={icono}
-        className={'icon_button'}
-        style={{ '--icon-color': color }}
+        className={styles["icon_button"]}
+        style={{ "--icon-color": color }}
       />
     </div>
   );
 }
 
 export default IconButton;
-
--- Scrum/fronted/src/Pages/Institution/InstitutionRoutes.jsx
+++ Scrum/fronted/src/Pages/Institution/InstitutionRoutes.jsx
@@ -1,68 +1,87 @@
-import './InstitutionRoutes.css'
-import MenuOption from '@components/Navs/TextMenuLink'
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from '@pages/Institution/InstitutionInfo/InstitutionDataProvider';
-import Comentarios from '@pages/Institution/Comments/Comments';
-import Requisitos from '@pages/Institution/Requirements/Requisitos';
-import Cita from '@pages/Institution/Appointments/Cita'
-import IconButton from '@components/Buttons/IconButton';
-import { faArrowLeft, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import "./InstitutionRoutes.css";
+import MenuOption from "@components/Navs/TextMenuLink";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "@pages/Institution/InstitutionInfo/InstitutionDataProvider";
+import Comentarios from "@pages/Institution/Comments/Comments";
+import Requisitos from "@pages/Institution/Requirements/Requisitos";
+import Cita from "@pages/Institution/Appointments/Cita";
+import IconButton from "@components/Buttons/IconButton";
+import {
+  faArrowLeft,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-const Informacion = ({data, ira}) => {
-  const {name_institutions} = data;
-  const [selectedSection, setSelectedSection] = useState('dashboard'); // Estado para controlar la sección
+const Informacion = ({ data, ira }) => {
+  const { name_institutions } = data;
+  const [selectedSection, setSelectedSection] = useState("dashboard"); // Estado para controlar la sección
 
   // Función para renderizar la sección según el estado
   const renderContent = () => {
     switch (selectedSection) {
-      case 'dashboard':
+      case "dashboard":
         return <Dashboard data={data} />;
-      case 'comentarios':
+      case "comentarios":
         return <Comentarios data={data} />;
-      case 'requisitos':
+      case "requisitos":
         return <Requisitos data={data} />;
-      case 'agendar':
+      case "agendar":
         return <Cita data={data} />;
       default:
         return <Dashboard data={data} />;
     }
-  }
+  };
 
   const openInNewTab = (url) => {
-    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
     if (newWindow) newWindow.opener = null;
   };
 
   return (
-    <div className='contenedor-info'>
-      <div className='header_institution'>
-        <IconButton icono={faArrowLeft} color='#0069AD' onclick={() => ira(true)} />
+    <div className="contenedor-info">
+      <div className="header_institution">
+        <IconButton
+          icono={faArrowLeft}
+          color="#0069AD"
+          onclick={() => ira(true)}
+        />
         <h3 className="titulo-info">{name_institutions}</h3>
-        
-        <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
+
+        <div
+          style={{ display: "flex", alignItems: "center", marginLeft: "auto" }}
+        >
           <p style={{ margin: 10 }}>Ver tramite pagina oficial</p>
-          
-          <IconButton 
-            icono={faArrowUpRightFromSquare} 
-            color='#0069AD' 
-            onclick={() => openInNewTab(data.url)} 
-            style={{ marginLeft: '8px' }} // Añade un margen para separar el icono del texto
+
+          <IconButton
+            icono={faArrowUpRightFromSquare}
+            color="#0069AD"
+            onclick={() => openInNewTab(data.url)}
+            style={{ marginLeft: "8px" }} // Añade un margen para separar el icono del texto
           />
         </div>
       </div>
-      <div className='menu-info'>
-        <MenuOption nombre='Información' onClick={() => setSelectedSection('dashboard')} />
-        <MenuOption nombre='Comentarios' onClick={() => setSelectedSection('comentarios')} />
-        <MenuOption nombre='Requisitos' onClick={() => setSelectedSection('requisitos')} />
-        <MenuOption nombre='Agendar' onClick={() => setSelectedSection('agendar')} />
+      <div className="menu-info">
+        <MenuOption
+          nombre="Información"
+          onClick={() => setSelectedSection("dashboard")}
+        />
+        <MenuOption
+          nombre="Comentarios"
+          onClick={() => setSelectedSection("comentarios")}
+        />
+        <MenuOption
+          nombre="Requisitos"
+          onClick={() => setSelectedSection("requisitos")}
+        />
+        <MenuOption
+          nombre="Agendar"
+          onClick={() => setSelectedSection("agendar")}
+        />
       </div>
 
-      <div className='informacion-contenido'>
-        {renderContent()}
-      </div>
+      <div className="informacion-contenido">{renderContent()}</div>
     </div>
   );
-}
+};
 
 export default Informacion;
