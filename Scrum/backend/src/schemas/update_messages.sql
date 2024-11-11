63887ef9e2466d7e58fec237c79cff78f8aa4f9c
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-11-11 04:20:32 -0600

    feat: cambios en bd para tomar en cuenta la hora

--- Scrum/backend/src/schemas/update_messages.sql
+++ Scrum/backend/src/schemas/update_messages.sql
@@ -1,1 +1,7 @@
-ALTER TABLE messages ADD COLUMN image_url character varying;
+ALTER TABLE messages ADD COLUMN image_url character varying; -- añadir columna para rutas  de imagenes
+
+/*
+Cambio de columna date para tomar en cuenta la hora
+*/
+ALTER TABLE messages
+ALTER COLUMN date TYPE timestamp;
