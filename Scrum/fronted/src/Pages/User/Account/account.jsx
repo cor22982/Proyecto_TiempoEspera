6981f8421ea7922baf200bc35299add4e81d8cb9
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-11-15 08:22:57 -0600

    fix: cambio de espacio entre informacion

--- Scrum/fronted/src/Pages/User/Account/account.jsx
+++ Scrum/fronted/src/Pages/User/Account/account.jsx
@@ -212,7 +212,7 @@ const Account = () => {
             }}
           >
             <p>
-              <strong>Email: </strong>
+              <strong>Email:</strong>
             </p>
             <Label_Input
               label_name={userData.email || "-No hay email-"}
@@ -232,7 +232,7 @@ const Account = () => {
             }}
           >
             <p>
-              <strong>Teléfono: </strong>
+              <strong>Teléfono:</strong>
             </p>
             <Label_Input
               label_name={userData.telephone || "-No hay telefono-"}
