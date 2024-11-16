a472ba07652c3a5b91c12ba84ac4ae500367a040
JosueSay <106031855+JosueSay@users.noreply.github.com>
2024-11-15 19:37:47 -0600

    feat: implementando lo borrado por chems

--- Scrum/fronted/src/Pages/User/Saved/Guardados.jsx
+++ Scrum/fronted/src/Pages/User/Saved/Guardados.jsx
@@ -1,25 +1,31 @@
 import SavedComponent from "@components/Cards/SavedProcedureCard/SavedProcedureCard";
 import { useState, useEffect } from "react";
 import PopUpSave from "@components/Modals/PopSuave/PopUpSave";
+import PopUpDeleteAppointment from "@components/Modals/PopUpDelete_Appointment/PopUpDelete_Appointment";
 import useApi from "@hooks/api/useApi";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import PopUpDeleteAppointment from "../../../Components/Modals/PopUpDelete_Appointment/PopUpDelete_Appointment";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
+import styles from "./Guardados.module.css";
 
 const Guardados = ({ pi }) => {
   const [showsave, setShowSave] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [information, setInformation] = useState({
     title: "",
     information: "",
     imagen: "",
     address: "",
   });
+  const [editDate, setEditDate] = useState("");
+  const [editTime, setEditTime] = useState("");
+  const [isEdited, setIsEdited] = useState(false);
+  const [appointmentToDelete, setAppointmentToDelete] = useState(null);
   const { llamadowithoutbody } = useApi(
     `https://deimoss.web05.lol/userAppointments/${pi}`
   );
   const [saved, setSaved] = useState([]);
-  const [appointmentToDelete, setAppointmentToDelete] = useState(null);
 
   const setValue = (name, value) => {
     setInformation((prevInfo) => ({
@@ -40,71 +46,112 @@ const Guardados = ({ pi }) => {
     setValue("title", save.name);
     setValue(
       "information",
-      `Se agendo una cita para ${save.institution_name} para el ${save.date} a las ${save.time}`
+      `Se agendó una cita para ${save.institution_name} para el ${save.date} a las ${save.time}`
     );
     setValue("imagen", save.imagen);
     setValue(
       "address",
-      `La direccion de la institucion es en  "${save.adress}"`
+      `La dirección de la institución es en "${save.adress}"`
     );
     setShowSave(true);
   };
 
-  // Eliminar cita
   const handleDelete = (appointmentId) => {
-    console.log("ID de cita recibido en handleDelete:", appointmentId);
-    setAppointmentToDelete(appointmentId); // Guardamos la cita seleccionada
-    console.log("Cita a eliminar:", appointmentId); // Asegúrate de que el ID se guarda correctamente
-    setShowDelete(true); // Activamos el PopUpDeleteAppointment
+    setAppointmentToDelete(appointmentId);
+    setShowDelete(true);
   };
 
-  // Confirmación de eliminación en el frontend
   const confirmDelete = async () => {
-    console.log("Cita a eliminar:", appointmentToDelete); // Verifica que 'appointmentToDelete' contiene el ID correcto
-
     try {
       const response = await fetch(
         `https://deimoss.web05.lol/appointment/${pi}`,
         {
           method: "DELETE",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ appointmentId: appointmentToDelete }), // Asegúrate de que 'appointmentToDelete' tenga el valor correcto
+          body: JSON.stringify({ appointmentId: appointmentToDelete }),
         }
       );
 
       const result = await response.json();
-      console.log("Respuesta del servidor:", result);
 
       if (result.success) {
-        setSaved(
-          (prevSaved) =>
-            prevSaved.filter(
-              (save) => save["id appointment"] !== appointmentToDelete
-            ) // Filtra la cita eliminada
+        setSaved((prevSaved) =>
+          prevSaved.filter((save) => save.id !== appointmentToDelete)
         );
-        alert("Cita eliminada con éxito.");
+        Swal.fire("Éxito", "Cita eliminada con éxito.", "success");
       } else {
-        console.error(
-          "Error al eliminar la cita: operación no exitosa en el backend."
-        );
-        alert("Hubo un error al eliminar la cita.");
+        Swal.fire("Error", "Hubo un problema al eliminar la cita.", "error");
       }
     } catch (error) {
-      console.error("Error en la solicitud DELETE:", error);
-      alert("Error en la solicitud DELETE.");
+      Swal.fire("Error", "Error en la solicitud DELETE.", "error");
     }
 
-    setShowDelete(false); // Cierra el popup después de confirmar la eliminación
+    setShowDelete(false);
   };
 
   const cancelDelete = () => {
-    setShowDelete(false); // Cerramos el popup sin eliminar
+    setShowDelete(false);
+  };
+
+  const handleEditDate = (save) => {
+    if (isEdited) {
+      Swal.fire("Error", "La fecha y hora ya han sido editadas.", "error");
+      return;
+    }
+
+    const [date, time] = save.date.split(" ");
+    setEditDate(date);
+    setEditTime(time);
+    setShowEditModal(true);
+  };
+
+  const handleSaveEditedDate = async (appointmentId) => {
+    const newDateTime = `${editDate} ${editTime}`;
+
+    try {
+      const response = await fetch(
+        `https://deimoss.web05.lol/appointments/${appointmentId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ date: newDateTime }),
+        }
+      );
+
+      if (response.ok) {
+        setSaved((prevSaved) =>
+          prevSaved.map((save) =>
+            save.id === appointmentId
+              ? { ...save, date: editDate, time: editTime }
+              : save
+          )
+        );
+        Swal.fire(
+          "¡Éxito!",
+          "Fecha y hora actualizadas correctamente.",
+          "success"
+        );
+        setIsEdited(true);
+      } else {
+        Swal.fire("Error", "No se pudo actualizar la cita.", "error");
+      }
+    } catch (error) {
+      Swal.fire("Error", "Error al actualizar la cita.", "error");
+    }
+
+    setShowEditModal(false);
+  };
+
+  const handleCancelEdit = () => {
+    setShowEditModal(false);
   };
 
   return (
-    <div style={{ padding: "10px", gap: "10px" }}>
+    <div className={styles.container}>
       {saved.map((save, index) => (
-        <div key={index} style={{ position: "relative", marginBottom: "5px" }}>
+        <div key={index} className={styles.cardContainer}>
           <SavedComponent
             image={save.imagen}
             title={save.name}
@@ -113,16 +160,15 @@ const Guardados = ({ pi }) => {
           />
           <FontAwesomeIcon
             icon={faTrash}
-            onClick={() => handleDelete(save["id appointment"])}
-            style={{
-              position: "absolute",
-              top: "10px",
-              right: "10px",
-              cursor: "pointer",
-              color: "red",
-              fontSize: "1.5rem",
-            }}
+            onClick={() => handleDelete(save.id)}
+            className={styles.deleteIcon}
             title="Eliminar cita"
+          />
+          <FontAwesomeIcon
+            icon={faEdit}
+            onClick={() => handleEditDate(save)}
+            className={styles.editIcon}
+            title="Editar fecha y hora"
           />
         </div>
       ))}
@@ -140,6 +186,51 @@ const Guardados = ({ pi }) => {
         confirmDelete={confirmDelete}
         cancelDelete={cancelDelete}
       />
+      {showEditModal && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modalContent}>
+            <h3>Editar Fecha y Hora</h3>
+            <div style={{ marginBottom: "10px" }}>
+              <label>Fecha:</label>
+              <input
+                type="date"
+                value={editDate}
+                onChange={(e) => setEditDate(e.target.value)}
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div>
+              <label>Hora:</label>
+              <input
+                type="time"
+                value={editTime}
+                onChange={(e) => setEditTime(e.target.value)}
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "15px",
+              }}
+            >
+              <button
+                onClick={handleCancelEdit}
+                className={`${styles.modalButton} ${styles.modalCancelButton}`}
+              >
+                Cancelar
+              </button>
+              <button
+                onClick={() => handleSaveEditedDate(save.id)}
+                className={`${styles.modalButton} ${styles.modalSaveButton}`}
+              >
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
